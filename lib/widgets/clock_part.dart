77c7289b414d747272d155c1c506c8f29e475cde
Nauzet Hernández <nauzet.aduen@gmail.com>
2020-10-14 14:32:08 +0100

    ADD animation/animController to roate

--- lib/widgets/clock_part.dart
+++ lib/widgets/clock_part.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class ClockPart extends StatelessWidget {
+class ClockPart extends StatefulWidget {
   final double angle;
   final Color color;
   final double width;
@@ -8,13 +8,37 @@ class ClockPart extends StatelessWidget {
   const ClockPart({this.angle, this.color, this.width});
 
   @override
+  _ClockPartState createState() => _ClockPartState();
+}
+
+class _ClockPartState extends State<ClockPart>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation<double> animation;
+
+  @override
+  void initState() {
+    controller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 60));
+    animation = CurvedAnimation(parent: controller, curve: Curves.linear);
+    controller.repeat();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Transform.rotate(
-      angle: angle,
+    return RotationTransition(
+      turns: animation,
       child: Container(
-        height: width,
-        color: color,
-        width: 150,
+        height: widget.width,
+        color: widget.color,
+        width: 75,
       ),
     );
   }
